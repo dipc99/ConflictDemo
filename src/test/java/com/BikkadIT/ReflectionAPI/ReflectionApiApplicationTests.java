5355c4e4027ea06c2b9269ada43a9654a86634bb
dipc99 <dipalichaudhari99@gmail.com>
2023-01-31 15:52:00 +0530

    NewProject

---
+++ src/test/java/com/BikkadIT/ReflectionAPI/ReflectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.ReflectionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
